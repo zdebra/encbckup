cd14efc90014b23415cf694719a2da55eb5fc499
zdebra <zd.brabec@gmail.com>
2023-02-18 20:28:39 +0100

    compress

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use core::panic;
+use std::io::{BufWriter, Write};
 
 fn main() {
     Cli::from_args(std::env::args().skip(1).collect());
@@ -20,10 +21,91 @@ impl Cli {
                 if !path.try_exists().expect("a valid path") {
                     panic!("path `{}` doesn't exist", path.display())
                 }
+
+                let cli = Self {};
+                if let Err(err) = cli.backup(path) {
+                    panic!("backup failed: {}", err);
+                }
+                println!("backup was successful!");
             }
             _ => panic!("invalid command"),
         }
     }
+
+    fn backup(&self, path: std::path::PathBuf) -> Result<(), String> {
+        if !path.is_file() {
+            panic!("currently handling files only!")
+        }
+        let file_origin = std::fs::File::open(path.clone()).unwrap();
+        let output_path = {
+            let filename = path
+                .components()
+                .last()
+                .unwrap()
+                .as_os_str()
+                .to_str()
+                .unwrap();
+            let mut it = path.components();
+            it.next_back().unwrap(); // remove last
+            let mut path = std::path::PathBuf::new();
+            path.extend(it);
+            path.extend(vec![filename.to_owned() + ".bkp"]);
+            path
+        };
+
+        let file_output = std::fs::File::create(output_path).unwrap();
+
+        match self.compress(file_origin, file_output) {
+            Err(e) => panic!("{}", e),
+            Ok(_) => {
+                println!("hehe");
+            }
+        }
+
+        Ok(())
+    }
+
+    fn compress<R: std::io::Read, W: std::io::Write>(&self, mut r: R, w: W) -> Result<(), String> {
+        let mut writer = brotli::CompressorWriter::new(
+            w, 4096, /* buffer size */
+            9,    /* compression levels 0-9 */
+            20,   /* recommended lg_window_size is between 20 and 22 */
+        );
+
+        let mut buf = [0u8; 4096];
+        loop {
+            match r.read(&mut buf) {
+                Err(e) => {
+                    if let std::io::ErrorKind::Interrupted = e.kind() {
+                        continue;
+                    }
+                    return Err(e.to_string());
+                }
+                Ok(size) => {
+                    if size == 0 {
+                        match writer.flush() {
+                            Err(e) => {
+                                if let std::io::ErrorKind::Interrupted = e.kind() {
+                                    continue;
+                                }
+                                return Err(e.to_string());
+                            }
+                            Ok(_) => break,
+                        }
+                    }
+                    match writer.write_all(&buf[..size]) {
+                        Err(e) => return Err(e.to_string()),
+                        Ok(_) => {}
+                    }
+                }
+            }
+        }
+        Ok(())
+    }
+
+    fn encrypt(&self) {}
+
+    fn upload(&self) {}
 }
 
 #[cfg(test)]
